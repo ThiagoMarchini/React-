09a34a16627d618f7ce7c1d4ca2970e033e04202
Thiago Marchini <thiago@msn.com>
2021-03-21 11:33:08 -0300

    finished react-tutorial

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App.js'
+import App from './Api.js'
 
 ReactDOM.render(<App />, document.getElementById('root'));
